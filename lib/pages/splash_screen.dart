2a5a07c03674e31779815b969f82133bf46b5af3
Pramudya <pramudyap333@gmail.com>
2025-04-10 21:30:44 +0700

    feat(ui): Update UI for Splash Page

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -13,25 +13,42 @@ class _SplashPageState extends State<SplashPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Container(
-          height: 400,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "assets/app_logo.png",
-                width: 150,
-                fit: BoxFit.contain,
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Expanded(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/app_logo.png",
+                  width: 150,
+                  fit: BoxFit.contain,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Text('POMODOT1MER',
+                    style: titleTextStyle.copyWith(
+                        fontSize: 20, fontWeight: bold)),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Setting Up Your Focus Session...',
+                  style: subtitleTextStyle.copyWith(fontSize: 12),
+                ),
+              ],
+            )),
+            Container(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: Text(
+                'Developed By Pramudya',
+                style: subtitleTextStyle.copyWith(fontSize: 12),
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              Text('POMODOT1MER',
-                  style:
-                      titleTextStyle.copyWith(fontSize: 20, fontWeight: bold)),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
