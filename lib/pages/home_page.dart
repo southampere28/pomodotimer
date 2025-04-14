03aee14aa7e0b88094f1485369655d945796d549
Pramudya <pramudyap333@gmail.com>
2025-04-14 23:07:42 +0700

    feat: Update UI Dynamic Message Tips and Button Start

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pomodotimer/theme.dart';
 import 'package:intl/intl.dart';
 import 'package:pomodotimer/widgets/cardlevel_widget.dart';
@@ -12,12 +15,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int menuActive = 0;
+  int menuActive = 0; // index menus
+  int currentIndex = 0; // Index showing tips Message
+  late Timer _timer; // timer for tips messages indexing
+
+  // Tips Message
+  List<String> tips = [
+    "Don't Waste Time for No Reason, Be Productive!",
+    "Focus on One Task at a Time.",
+    "Set Clear Goals and Prioritize Your Tasks.",
+    "Take Breaks to Refresh Your Mind and Body.",
+    "Stay Organized and Plan Your Day.",
+    "Use the Pomodoro Technique for Maximum Focus.",
+    "Keep Your Workspace Clean for Better Productivity.",
+    "Don't Procrastinate, Take Action Now!",
+    "Remember: Time is Your Most Valuable Asset.",
+    "Consistency is Key to Achieving Your Goals."
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    // Mengatur timer untuk mengubah teks setiap 3 detik
+    _timer = Timer.periodic(Duration(seconds: 3), (timer) {
+      setState(() {
+        // Update index untuk mengganti teks
+        currentIndex = (currentIndex + 1) % tips.length;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // Hentikan timer ketika widget dihapus dari tree
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // var format = DateFormat.yMd('id');
-    // var dateString = format.format(DateTime.now());
+    var format = DateFormat("EEEE, dd/MM/yyyy");
+    var dateString = format.format(DateTime.now());
 
     return SafeArea(
       child: Scaffold(
@@ -39,7 +77,7 @@ class _HomePageState extends State<HomePage> {
                         style: titleTextStyle.copyWith(
                             fontSize: 14, fontWeight: bold),
                       ),
-                      Text('Monday 09/04/25',
+                      Text(dateString,
                           style: primaryTextStyle.copyWith(
                               fontSize: 12, fontWeight: regular)),
                     ],
@@ -91,7 +129,7 @@ class _HomePageState extends State<HomePage> {
                         arrayContent: const [
                           "work 25 min",
                           "break 5 min",
-                          "repeat 2x",
+                          "repeat 3x",
                           "work 25 min",
                           "long break 30 min"
                         ],
@@ -114,19 +152,19 @@ class _HomePageState extends State<HomePage> {
                         });
                       },
                       child: CardLevel(
-                        title: 'Medium',
+                        title: 'Hard',
                         arrayContent: const [
                           "work 25 min",
                           "break 5 min",
-                          "repeat 2x",
+                          "repeat 3x",
                           "work 25 min",
-                          "long break 30 min"
+                          "long break 15 min"
                         ],
                         isActive: menuActive == 2,
                       ),
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 10,
                   ),
                   Expanded(
@@ -136,23 +174,13 @@ class _HomePageState extends State<HomePage> {
                           menuActive = 3;
                         });
                       },
-                      child: CardLevel(
-                        title: 'Custom',
-                        arrayContent: const [
-                          "work 25 min",
-                          "break 5 min",
-                          "repeat 2x",
-                          "work 25 min",
-                          "long break 30 min"
-                        ],
-                        isActive: menuActive == 3,
-                      ),
+                      child: CardLevelCustom(isActive: menuActive == 3),
                     ),
                   ),
                 ],
               ),
               const SizedBox(
-                height: 8,
+                height: 12,
               ),
               RichText(
                 text: TextSpan(
@@ -161,11 +189,60 @@ class _HomePageState extends State<HomePage> {
                       primaryTextStyle.copyWith(fontSize: 10, fontWeight: bold),
                   children: [
                     TextSpan(
-                        text: 'Dont Waste Time for No Reason Be Productive!',
-                        style: primaryTextStyle.copyWith(
+                        text: tips[currentIndex],
+                        style: subtitleTextStyle.copyWith(
                             fontSize: 10, fontWeight: regular)),
                   ],
                 ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        Fluttertoast.showToast(msg: 'Test');
+                      },
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(whiteColor),
+                          foregroundColor:
+                              MaterialStateProperty.resolveWith<Color?>(
+                            (Set<MaterialState> states) {
+                              if (states.contains(MaterialState.pressed)) {
+                                return whiteColor; // Warna saat ditekan
+                              }
+                              return blackColor;
+                            },
+                          ),
+                          overlayColor:
+                              MaterialStateProperty.resolveWith<Color?>(
+                            (Set<MaterialState> states) {
+                              if (states.contains(MaterialState.pressed)) {
+                                return blackActiveColor; // Warna saat ditekan
+                              }
+                              return null; // default
+                            },
+                          ),
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              side:
+                                  const BorderSide(color: blackColor, width: 2),
+                            ),
+                          ),
+                          elevation: MaterialStateProperty.all(4),
+                          shadowColor: MaterialStateColor.resolveWith(
+                              (states) => Colors.black)),
+                      child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 4),
+                          child: Text('Start Now',
+                              style: (GoogleFonts.poppins().copyWith(
+                                  fontSize: 14, fontWeight: medium))))),
+                ],
               )
             ],
           ),
